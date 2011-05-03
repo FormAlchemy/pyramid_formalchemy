2dd401112785cee2c877420d3993cdff908febd7
Gael Pasgrimaud <gael@gawel.org>
2011-05-03 17:50:29 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid>=1.0', 'WebError', 'FormAlchemy>=1.3.7']
+requires = ['pyramid>=1.0', 'WebError', 'FormAlchemy>=1.3.8']
 
 setup(name='pyramid_formalchemy',
-      version='0.2',
+      version='0.3',
       description='FormAlchemy plugins and helpers for Pyramid',
       long_description=README + '\n\nCHANGES\n=======\n\n' +  CHANGES,
       classifiers=[
