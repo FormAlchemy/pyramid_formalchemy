a178178aace57184c6c971e751a5787c8fe76485
Gael Pasgrimaud <gael@gawel.org>
2011-12-23 12:42:37 +0100

    add zope.component as a dependencie

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid>=1.1', 'WebError', 'FormAlchemy>=1.3.8', 'Babel']
+requires = ['pyramid>=1.1', 'WebError', 'FormAlchemy>=1.3.8', 'Babel',
+            'zope.component']
 
 setup(name='pyramid_formalchemy',
       version='0.4.3dev',
