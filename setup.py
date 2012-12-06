80771cafbce2eb317024c3361b3566577615d73d
Nathan Van Gheem <vangheem@gmail.com>
2012-12-06 11:57:52 -0600

    fix pyramid compat issue fixes #19

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = ['pyramid>=1.1', 'WebError', 'FormAlchemy>=1.3.8', 'Babel',
             'zope.component']
 
-version = '0.4.3'
+version = '0.4.4dev'
 
 setup(name='pyramid_formalchemy',
       version=version,
